be0aa3714c49239589e5e5868fc677d16f192a56
zchen <chenzze1@student.ubc.ca>
2022-11-20 11:46:28 -0800

    complete assignment 3

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
+# STAT 545B - Assignment 3
 
+This is the repository for STAT 545B - Assignment 3 - Option A - BC Liquor app.
+
+Visit the app at https://czz1997.shinyapps.io/bcl_new/.
+
+### New Features
+
+1. Allow users to decide the color of the bars in the plot.\
+This feature could be useful if users want to collect plots under different filter and
+want to distinguish them via different colors.
+
+2. Sort data by price.\
+Originally the data does not have any order, and providing the ability to sort data by price
+can help discover the relationship between price and other variables and facilitate adjusting
+the price range filter.
+
+3. Separate plot and table tabs.\
+Putting plot and table in different tabs can improve the UI to make the app
+more visually pleasant and easy to use
+
+4. Interactive table\
+Interactive table provides searching, paging and sorting abilities for users
+to further explore the results.
+
+### Acknowledgements
+
+This app is based on the BC Liquor app by Dean Attali. The dataset used by the app is from OpenDataBC.
--- bcl_new/app.R
+++ bcl_new/app.R
@@ -14,6 +14,20 @@ library(dplyr)
 bcl <- read_csv("https://raw.githubusercontent.com/daattali/shiny-server/master/bcl/data/bcl-data.csv")
 options(shiny.autoreload = TRUE)
 
+# Features added to the basic BC Liquor shiny app:
+# 1. allow users to decide the color of the bars in the plot.
+# This feature could be useful if users want to collect plots under different filter and
+# want to distinguish them via different colors.
+# 2. sort data by price.
+# Originally the data does not have any order, and providing the ability to sort data by price
+# can help discover the relationship between price and other variables and facilitate adjusting
+# the price range filter.
+# 3. separate plot and table tabs.
+# Putting plot and table in different tabs can improve the UI to make the app
+# more visually pleasant and easy to use
+# 4. interactive table
+# Interactive table provides searching, paging and sorting abilities for users
+# to further explore the results.
 
 ui <- fluidPage(
   titlePanel("BC Liquor Store prices"),
@@ -23,12 +37,18 @@ ui <- fluidPage(
       radioButtons("typeInput", "Product type",
                    choices = c("BEER", "REFRESHMENT", "SPIRITS", "WINE"),
                    selected = "WINE"),
-      uiOutput("countryOutput")
+      uiOutput("countryOutput"),
+      # Feature 1: allow users to decide the color of the bars in the plot
+      colourpicker::colourInput("colorInput", "Plot color", "Black"),
+      # Feature 2: sort data by price
+      checkboxInput("sortInput", "Sort results by price", TRUE)
     ),
     mainPanel(
-      plotOutput("coolplot"),
-      br(), br(),
-      tableOutput("results")
+      # Feature 3: separate plot and table tabs
+      tabsetPanel(
+        tabPanel("Plot", plotOutput("coolplot")),
+        tabPanel("Table", DT::dataTableOutput("results"))# Feature 4: interactive table
+      )
     )
   )
 )
@@ -45,12 +65,21 @@ server <- function(input, output) {
       return(NULL)
     }
 
-    bcl %>%
-      filter(Price >= input$priceInput[1],
-             Price <= input$priceInput[2],
-             Type == input$typeInput,
-             Country == input$countryInput
-      )
+    unsorted <-
+      bcl %>%
+        filter(Price >= input$priceInput[1],
+               Price <= input$priceInput[2],
+               Type == input$typeInput,
+               Country == input$countryInput
+        )
+
+    # Feature 2: sort data by price
+    if (input$sortInput) {
+      unsorted %>%
+        arrange(Price)
+    } else {
+      unsorted
+    }
   })
 
   output$coolplot <- renderPlot({
@@ -58,10 +87,11 @@ server <- function(input, output) {
       return()
     }
     ggplot(filtered(), aes(Alcohol_Content)) +
-      geom_histogram()
+      geom_histogram(fill=input$colorInput) # Feature 1: allow users to decide the color of the bars in the plot
   })
 
-  output$results <- renderTable({
+  # Feature 4: interactive table
+  output$results <- DT::renderDataTable({
     filtered()
   })
 }
